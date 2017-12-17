b7faaf095162af60c831b8c827eb9f916b9fde35
smawanzi <34251833+smawanzi@users.noreply.github.com>
2017-12-17 14:28:14 +0100

    Add files via upload

---
+++ voiture/voiture/Voiture.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace voiture
+{
+   public class Voiture
+    {
+        //données membres
+        string constructeur;
+        string marque;
+        string numImmatriculation;
+        DateTime dateMiseCirculation;
+        int nbPortes;
+        int Energie;
+        decimal prix;
+        int kilometrage;
+        int vitesse;
+        int capaciteReservoir;
+        bool cleSurVolant;
+        bool ceintureBouclee;
+       // méthodes pour accéder aux données
+        public string getNumImmat()
+        {
+            return numImmatriculation;
+        }
+        public void setNumImmat(string numImmat)
+        {
+            numImmatriculation = numImmat;
+        }
+       // constructeur
+        public Voiture(string construct, string marq, string num)
+        {
+            constructeur = construct;
+            marque = marq;
+            numImmatriculation = num;
+        }
+        public Voiture(string construct, string marq, string num,int nbp, DateTime mcircul)
+        {
+            constructeur = construct;
+            marque = marq;
+            numImmatriculation = num;
+            nbPortes = nbp;
+            dateMiseCirculation = mcircul;
+        }
+            public void affiche()
+        {
+        Console.WriteLine("véhicule de constructeur {0} de marque {1} n° d'immatriculation {2} roule à {3}", constructeur, marque, numImmatriculation, vitesse);
+	    Console.ReadLine();
+        }
+            public void ceintureBoucle()
+        {
+            ceintureBouclee = false;
+
+        }
+            public void démarrer()
+        {
+            vitesse = 10;
+        }
+            public void accélérer()
+        {
+
+        }
+    }
+}
---
+++ voiture/voiture/utilisationVoiture.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace voiture
+{
+    class utilisationVoiture
+    {
+        static void Main(string[] args)
+        {
+            // saisie des informations du véhicule;
+            string construct;
+            string marq;
+            string num;
+            int nbp;
+            DateTime mcircul = new DateTime(2016, 01, 07);
+            Console.WriteLine("saisissez le constructeur");
+            construct = Console.ReadLine();
+
+            Console.WriteLine("saisissez la marque");
+            marq = Console.ReadLine();
+
+            Console.WriteLine("saisissez le numéro d'immatriculation");
+            num = Console.ReadLine();
+
+
+
+
+            Voiture v1 = new Voiture(construct, marq, num);
+            v1.affiche();
+            Console.WriteLine("n° d'immatriculation : {0}",v1.getNumImmat());
+
+            Console.WriteLine("saisissez le constructeur");
+            construct = Console.ReadLine();
+
+            Console.WriteLine("saisissez la marque");
+            marq = Console.ReadLine();
+
+            Console.WriteLine("saisissez le numéro d'immatriculation");
+            num = Console.ReadLine();
+            Console.WriteLine("saisissez le nb portes");
+            nbp = Int32.Parse(Console.ReadLine());
+
+            Voiture v2 = new Voiture(construct, marq, num, nbp, mcircul);
+
+            Console.WriteLine("n° d'immatriculation : {0}", v2.getNumImmat());
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
